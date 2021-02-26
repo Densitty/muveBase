6585f2a13401558ad34899f318dd20969b4559ea
Densitty <dennis.brough@yandex.com>
2021-02-26 13:58:23 -0600

    making search available functionality

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const request = require("superagent");
 
 const apiKey = "7c53bd46706298a65e4a124494847e2b";
 const baseUrl = `https://api.themoviedb.org/3`;
-const url = `api.themoviedb.org/3/movie/550?api_key=${apiKey}`;
 const nowPlaying = `${baseUrl}/movie/now_playing?api_key=${apiKey}`;
 const image = `https://image.tmdb.org/t/p/w300`;
 
@@ -48,6 +47,29 @@ router.get("/movie/:id", async (req, res, next) => {
     });
   } catch (e) {
     console.log("Error ", e);
+  }
+});
+
+router.post("/search", async (req, res, next) => {
+  // since we are sending data over the wire, express.urlencoded() makes it possible for us to read the data in forms
+  const searchTerm = encodeURI(req.body.movieSearch);
+  const category = req.body.cat;
+  const searchUrl = `${baseUrl}/search/${category}?query=${searchTerm}&api_key=${apiKey}`;
+  try {
+    const result = await request.get(searchUrl);
+    // we can also do below;
+    // const movieData = JSON.parse(result.text)
+    const data = result.body;
+    // console.log(req.query);
+    // res.json(data);
+    if (category === "person") {
+      data.results = data.results[0].known_for;
+    }
+    res.render("index", {
+      movies: data.results,
+    });
+  } catch (e) {
+    console.log(e);
   }
 });
 
